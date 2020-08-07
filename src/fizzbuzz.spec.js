10c71dfe68395fd89e63295936c4f8964cea5ffa
Stephen Cavaliere <stephencavaliere@gmail.com>
2020-08-07 15:22:32 -0400

    small deduplication in spec

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,33 +1,26 @@
 const assert = require('assert');
+const fizzbuzz = require('./fizzbuzz');
 
 describe('fizz buzz', () => {
   describe('#evaluate', () => {
     it('should return the number 1 when given 1', () => {
-      const fizzbuzz = require('./fizzbuzz');
-
       const actual = fizzbuzz.evaluate(1);
 
       assert.equal(actual, '1');
     });
     it('should return the number 2 when given 2', () => {
-      const fizzbuzz = require('./fizzbuzz');
-
       const actual = fizzbuzz.evaluate(2);
 
       assert.equal(actual, '2');
     });
 
     it('should return "Fizz" when given 3', () => {
-      const fizzbuzz = require('./fizzbuzz');
-
       const actual = fizzbuzz.evaluate(3);
 
       assert.equal(actual, 'Fizz');
     });
 
     it('should return "Buzz" when given 5', () => {
-      const fizzbuzz = require('./fizzbuzz');
-
       const actual = fizzbuzz.evaluate(5);
 
       assert.equal(actual, 'Buzz');
